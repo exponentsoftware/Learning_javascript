0565925deb92a23c678765b992b8154ed6b70dd2
Pradeep Kumar Sahani <mark42virus>
2021-01-13 14:57:41 +0530

    updation

--- test.js
+++ test.js
@@ -4,3 +4,10 @@ var date = Date(); // this is the date function that relate to the current date.
 
 console.log(data);
 console.log(date);
+
+
+function test()
+{
+    document.getElementById('demo').innerHTML = "hello Pradeep Sahani"
+    
+}
