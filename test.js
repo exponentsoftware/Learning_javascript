f97222662582f4aaa6a2b50f95387787e56c7162
Pradeep Kumar Sahani <mark42virus>
2021-01-13 14:47:26 +0530

    updation

---
+++ test.js
@@ -0,0 +1,6 @@
+var data = "This is pradeep which is from siddharthnagar"
+
+var date = Date(); // this is the date function that relate to the current date. 
+
+console.log(data);
+console.log(date);
