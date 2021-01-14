13081f973c9d82c176faafff4d4468befb60595a
Pradeep Kumar Sahani <mark42virus>
2021-01-14 23:01:40 +0530

    added

--- test.js
+++ test.js
@@ -1,13 +1,20 @@
-var data = "This is pradeep which is from siddharthnagar"
+// var data = "This is pradeep which is from siddharthnagar"
 
-var date = Date(); // this is the date function that relate to the current date. 
+// var date = Date(); // this is the date function that relate to the current date. 
 
-console.log(data);
-console.log(date);
+// console.log(data);
+// console.log(date);
 
 
-function test()
-{
-    document.getElementById('demo').innerHTML = "hello Pradeep Sahani"
+// function test()
+// {
+//     document.getElementById('demo').innerHTML = "hello Pradeep Sahani"
     
+// }
+
+
+function pradeep(data1, data2)
+{
+    console.log(data1*data2);
 }
+pradeep(5,6)
